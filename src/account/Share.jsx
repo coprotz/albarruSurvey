759463c5ecd4b88aaa9b8e90e5eff351f5d2826a
coprotz <qsallya@gmail.com>
2022-04-07 20:42:15 +0300

    second

--- src/account/Share.jsx
+++ src/account/Share.jsx
@@ -2,16 +2,24 @@ import React from 'react'
 import {motion} from 'framer-motion'
 import {
   EmailShareButton,
+  EmailIcon,
   FacebookShareButton,
   InstapaperShareButton, 
   MailruShareButton,
+  MailruIcon,
   PocketShareButton,
   TelegramShareButton,
   TwitterShareButton,
   ViberShareButton,
   WhatsappShareButton,
   WhatsappIcon,
-  WorkplaceShareButton
+  TwitterIcon,
+  WorkplaceShareButton,
+  FacebookMessengerIcon,
+  FacebookIcon,
+  InstapaperIcon,
+  TelegramIcon,
+  // TelegramShareButton
 } from "react-share";
 
 const Share = () => {
@@ -24,12 +32,33 @@ const Share = () => {
         transition={{ ease: "easeOut", duration: 0.5 }} 
         className="account_body">
           <div className="share_inner">
-              <h2>Share your questionnaire to your respondents</h2>
+              <h2>Share your questionnaire to your respondents through the following platforms;</h2>
               <div className="shared_icons">
                 <WhatsappShareButton
                   url={shareUrl}                  >
                     <WhatsappIcon size={40} round={true}/>
                 </WhatsappShareButton>
+                <EmailShareButton>
+                    <EmailIcon size={40} round={true}/>
+                </EmailShareButton>
+                <MailruShareButton>
+                  <MailruIcon size={40} round={true}/>
+                </MailruShareButton>
+                <TwitterShareButton>
+                  <TwitterIcon size={40} round={true}/>
+                </TwitterShareButton>
+                <FacebookShareButton>
+                  <FacebookMessengerIcon size={40} round={true}/>
+                </FacebookShareButton>
+                <FacebookShareButton>
+                  <FacebookIcon size={40} round={true}/>
+                </FacebookShareButton>
+                {/* <InstapaperShareButton>
+                  <InstapaperIcon size={40} round={true}/>
+                </InstapaperShareButton> */}
+                <TelegramShareButton>
+                  <TelegramIcon size={40} round={true}/>
+                </TelegramShareButton>
 
               </div>
           </div>
